5ac2b37d3b4b13486f0cc75c1612919142667b85
Marshall Johnson <marshalljohnson193@gmail.com>
2020-10-19 12:16:33 -0600

    init map

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
-require('dotenv')
+require('dotenv').config()
 const express = require('express')
 const massive = require('massive')
 const express = require('express-session')
 const app = express()
 const authCtrl = ('./controllers/authController.js')
 const profileCtrl = ('./controllers/profileController.js')
+const weatherCtrl = ('./controllers/weatherController.js')
+
 app.use(express.json())
 
 //* Auth Contollers
@@ -14,6 +16,9 @@ app.post('/api/auth/logout', authCtrl.logout)
 app.get('/api/auth/getUser', authCtrl.getUser)
 
 //* Profile Controllers
-app.post('/api/users/:userid/favorites', addBeach)
-app.delete('/api/users/:userid/favorites/:favoriteid', deleteBeach)
-app.get('/api/users/:userid/favorites/', getFavoriteBeaches)
+app.post('/api/users/:userid/favorites', profileCtrl.addBeach)
+app.delete('/api/users/:userid/favorites/:favoriteid', profileCtrl.deleteBeach)
+app.get('/api/users/:userid/favorites/', profileCtrl.getFavoriteBeaches)
+
+//* Weather Contoller
+app.get('/api/weather', weatherCtrl.getWeather)
