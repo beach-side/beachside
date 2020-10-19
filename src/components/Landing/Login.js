50e3f3ec2b23a89c9f1e8f5fd6f20d968b8ab482
Kara LeSueur <kara.lesueur@gmail.com>
2020-10-19 15:54:02 -0700

    starter setup of Login

---
+++ src/components/Landing/Login.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'
+
+function Landing() {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    
+
+    return (
+        <div className='landing'> 
+            Login
+
+            <div className='login-view'>
+                
+            </div>
+
+        </div>
+    )
+}
+
+export default Landing
