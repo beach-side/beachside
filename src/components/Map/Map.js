2c98a9ff7ca504e7b70074f060ceb41b855952d1
Marshall Johnson <marshalljohnson193@gmail.com>
2020-10-20 18:24:51 -0600

    before search and infobox

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState, useRef, useCallback } from 'react'
 import {
     GoogleMap,
     useLoadScript,
@@ -20,11 +20,21 @@ const options = {
 }
 
 
+
 function Map() {
     const { isLoaded, loadError } = useLoadScript({
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
         libraries,
     })
+
+    const [markers, setMarkers] = useState([])
+    const [selected, setSelected] = useState(null)
+
+    const mapRef = useRef();
+
+    const onMapLoad = useCallback((map) => {
+        mapRef.current = map;
+    }, [])
 
     if (loadError) return "Error loading maps"
     if (!isLoaded) return "Loading Maps"
@@ -38,9 +48,24 @@ function Map() {
                 center={center}
                 options={options}
                 onClick={(e) => {
-                    console.log(e)
+                    setMarkers(current => [...current, {
+                        lat: e.latLng.lat(),
+                        lng: e.latLng.lng()
+                    }])
                 }}
+                onLoad={onMapLoad}
             >
+                {markers.map((marker, i) => {
+                    return <Marker
+                        key={i}
+                        position={{ lat: marker.lat, lng: marker.lng }}
+                        onClick={() => {
+                            setSelected(marker)
+                        }}
+                    />
+                })}
+
+                {selected ? (<InfoWindow></InfoWindow>) : null}
             </GoogleMap>
         </div>
     )
